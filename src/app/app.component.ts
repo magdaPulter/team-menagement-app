85fb2e40b47a0dac97e537fce3dd67cb2f1446ea
magdaPulter <magda.pulter@gmail.com>
2024-01-02 18:26:48 +0100

    project using standalone APIs bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component, Input } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MainContinerComponent } from "./components/main-continer/main-continer.component";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: true,
+    imports: [RouterOutlet, MainContinerComponent]
 })
 export class AppComponent {
   title = 'ng-bootstrap-starter-kit';
